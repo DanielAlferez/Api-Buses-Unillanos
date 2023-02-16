04938edfb655e24d85f2cc3dfe01c7adbcb63416
DanielAlferez <alferezgarciadanielcamilo@gmail.com>
2023-02-15 20:13:45 -0500

    models changed & end points added

--- src/app.py
+++ src/app.py
@@ -52,7 +52,6 @@ def register_user():
     new_user = User(email=email, name=name, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
-    
     return jsonify({
         "id": new_user.id,
         "name": new_user.name,
@@ -168,6 +167,143 @@ def create_hour():
     return jsonify({
         "message": "Hora añadida exitosamente"
     })
+
+
+@app.route("/locations", methods=["GET"])
+def get_locations():
+    locations = Location.query.all()
+    serialized_locations = []
+    for location in locations:
+        serialized_location = {
+            'locations_id': location.location_id,
+            'locations_name': location.location_name,
+            'latitude': location.latitude,
+            'longitude': location.longitude,
+            'bus_id': location.bus_id,
+        }
+        serialized_locations.append(serialized_location)
+
+    return jsonify(serialized_locations)
+
+
+@app.route("/locations/<int:location_id>", methods=["GET"])
+def get_location(location_id):
+
+    location = Location.query.filter_by(location_id=location_id).first()
+
+    return jsonify({
+        'location_id': location.location_id,
+        'location_name': location.location_name,
+        'latitude': location.latitude,
+        'longitude': location.longitude,
+        'bus_id': location.bus_id,
+    })
+
+
+@app.route("/create-location", methods=["POST"])
+def create_location():
+    location_name = request.json["location_name"]
+    latitude = request.json["latitude"]
+    longitude = request.json["longitude"]
+    bus_id = request.json["bus_id"]
+
+    location_exists = Location.query.filter_by(location_name=location_name).first() is not None
+
+    if location_exists:
+        return jsonify({"error": "Ubicación ya existe"}), 409
+    
+    new_location = Location(location_name=location_name, latitude=latitude, longitude=longitude, bus_id=bus_id)
+    db.session.add(new_location)
+    db.session.commit()
+    
+    return jsonify({
+        "message": "Ubicación añadida exitosamente"
+    })
+
+
+@app.route("/buses_hours", methods=["GET"])
+def get_buses_hours():
+    buses_hours = BusesHours.query.all()
+    serialized_buses_hours = []
+    for bus_hour in buses_hours:
+        serialized_bus_hour = {
+            'bus_id': bus_hour.bus_id,
+            'hour_id': bus_hour.hour_id
+        }
+        serialized_buses_hours.append(serialized_bus_hour)
+
+    return jsonify(serialized_buses_hours)
+
+
+@app.route("/buses_hours/<int:bus_id>", methods=["GET"])
+def get_bus_hour(bus_id):
+
+    bus_hours = BusesHours.query.filter_by(bus_id=bus_id).all()
+
+    serialized_buses_hours = []
+    for bus_hour in bus_hours:
+        serialized_bus_hour = {
+            'hour_id': bus_hour.hour_id,
+        }
+        serialized_buses_hours.append(serialized_bus_hour)
+
+    return jsonify({
+        'bus_id': bus_hour.bus_id,
+        'hours': serialized_buses_hours
+    })
+
+
+@app.route("/create_bus_hour", methods=["POST"])
+def create_bus_hour():
+    bus_id = request.json["bus_id"]
+    hour_id = request.json["hour_id"]
+    
+    new_bus_hour = BusesHours(bus_id=bus_id, hour_id=hour_id)
+    db.session.add(new_bus_hour)
+    db.session.commit()
+    
+    return jsonify({
+        "message": "Hora añadida exitosamente al bus"
+    })
+
+
+@app.route("/info_buses", methods=["GET"])
+def get_info_buses():
+
+    buses = Buses.query.all()
+
+    serialized_buses = []
+    for bus in buses:
+        bus_hours = BusesHours.query.filter_by(bus_id=bus.bus_id).all()
+        serialized_buses_hours = []
+        for bus_hour in bus_hours:
+            hours = Hours.query.filter_by(hour_id=bus_hour.hour_id).first()
+            serialized_bus_hour = {
+                'hour_id': bus_hour.hour_id,
+                'hour_name': hours.hour_name
+            }
+            serialized_buses_hours.append(serialized_bus_hour)
+
+        locations = Location.query.filter_by(bus_id=bus.bus_id).all()
+        serialized_locations = []
+        for location in locations:
+            serialized_location = {
+                'location_id': location.location_id,
+                'location_name': location.location_name,
+                'latitude': location.latitude,
+                'longitude': location.longitude,
+            }
+            serialized_locations.append(serialized_location)
+
+        serialized_bus = {
+            'bus_id': bus.bus_id,
+            'bus_name': bus.bus_name,
+            'hours': serialized_buses_hours,
+            'locations': serialized_locations
+        }
+        serialized_buses.append(serialized_bus)
+
+    return serialized_buses
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/models.py
+++ src/models.py
@@ -8,35 +8,29 @@ def get_uuid():
     return uuid4().hex
 
 class User(db.Model):
-    __tablename__ = "users"
     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
     name = db.Column(db.String(100))
     email = db.Column(db.String(345), unique=True)
     password = db.Column(db.Text, nullable=False)
 
 class Buses(db.Model):
-    __tablename__ = "buses"
-    bus_id = db.Column(db.Integer, Sequence('bus_id_seq'), primary_key=True, unique=True)
+    bus_id = db.Column(db.Integer, primary_key=True)
     bus_name = db.Column(db.String(100), unique=True, nullable=False)
 
 class Location(db.Model):
-    __tablename__ = "locations"
-    location_id = db.Column(db.Integer, Sequence('location_id_seq'), primary_key=True, unique=True)
+    location_id = db.Column(db.Integer, primary_key=True)
     location_name = db.Column(db.String(100), unique=True, nullable=False)
     latitude = db.Column(db.String(50), nullable=False)
     longitude = db.Column(db.String(50), nullable=False)
+    bus_id = db.Column(db.Integer, db.ForeignKey('buses.bus_id'), nullable=False)
+    bus = db.relationship('Buses', uselist=True)
 
 class Hours(db.Model):
-    __tablename__ = "hours"
-    hour_id = db.Column(db.Integer, Sequence('hour_id_seq'), primary_key=True, unique=True)
+    hour_id = db.Column(db.Integer, primary_key=True)
     hour_name = db.Column(db.String(100), unique=True, nullable=False)
-
+    
 class BusesHours(db.Model):
-    __tablename__ = "buses_hours"
-    bus_id = db.Column(db.Integer, db.ForeignKey('buses'), primary_key=True)
-    hour_id = db.Column(db.Integer, db.ForeignKey('hours'), primary_key=True)
-
-class BusesLocations(db.Model):
-    __tablename__ = "buses_locations"
-    bus_id = db.Column(db.Integer, db.ForeignKey('buses'), primary_key=True)
-    location_id = db.Column(db.Integer, db.ForeignKey('locations'), primary_key=True)
+    bus_id = db.Column(db.Integer, db.ForeignKey('buses.bus_id'), nullable=False, primary_key=True)
+    bus = db.relationship('Buses', uselist=True)
+    hour_id = db.Column(db.Integer, db.ForeignKey('hours.hour_id'), nullable=False, primary_key=True)
+    hour = db.relationship('Hours', uselist=True)
