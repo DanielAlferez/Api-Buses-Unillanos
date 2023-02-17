df4eb13c44e553c07083961f06f75a212affea5a
Daniel Camilo Alférez García <68829145+DanielAlferez@users.noreply.github.com>
2023-02-17 13:40:01 -0500

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,86 +1,86 @@
-INSERT INTO buses (bus_name) VALUES("Viva");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Esquina semáforo de Viva", "4.125370", "-73.636071", 1);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Boutique Revival", "4.124263", "-73.634550", 1);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Casa Jeréz Inmobiliaria", "4.123136","-73.6321427", 1);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Calle 33-45 esquina", "4.1228957","-73.6296829", 1);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Panadería y pastelería La Cosmopolita", "4.1191422","-73.6284305", 1);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Carrera 28F esquina", "4.1198703","-73.6257133", 1);
+INSERT INTO buses (bus_name) VALUES('Viva');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Esquina semáforo de Viva', '4.125370', '-73.636071', 1);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Boutique Revival', '4.124263', '-73.634550', 1);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Casa Jeréz Inmobiliaria', '4.123136','-73.6321427', 1);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Calle 33-45 esquina', '4.1228957','-73.6296829', 1);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Panadería y pastelería La Cosmopolita', '4.1191422','-73.6284305', 1);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Carrera 28F esquina', '4.1198703','-73.6257133', 1);
 
-INSERT INTO buses (bus_name) VALUES("Villacentro");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Frente Llanocentro","4.1331923","-73.6366531", 2);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Comidas rapidas mazorcas", "4.1333947", "-73.6353101", 2);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Paradero busetas Dóminos", "4.1324333", "-73.6324242", 2);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Droguería la rebaja", "4.1314713", "-73.6295618", 2);
+INSERT INTO buses (bus_name) VALUES('Villacentro');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Frente Llanocentro','4.1331923','-73.6366531', 2);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Comidas rapidas mazorcas', '4.1333947', '-73.6353101', 2);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Paradero busetas Dóminos', '4.1324333', '-73.6324242', 2);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Droguería la rebaja', '4.1314713', '-73.6295618', 2);
 
-INSERT INTO buses (bus_name) VALUES("Porfia");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Afueras bloque apartamentos", "4.0593938", "-73.6700653", 3);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Supermercado Marketing", "4.064833", "-73.671318", 3);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Salida los Chopos", "4.073257", "-73.669342", 3);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Congente", "4.079063", "-73.669262", 3);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Consuerte", "4.0826917", "-73.6692422", 3);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Bomba Texaco", "4.088458", "-73.670179", 3);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Segunda entrada Samán", "4.091312", "-73.666702", 3);
-
-
-INSERT INTO buses (bus_name) VALUES("Grama");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Cuatro Parques", "4.158367", "-73.640998", 4);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Picadas y algo más", "4.157906", "-73.637702", 4);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Frente Bomba Texaco", "4.156651", "-73.632235", 4);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Droguería la Providencia", "4.151178", "-73.628527", 4);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Yamaha Jordán", "4.148175", "-73.624406", 4);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Paradero busetas COFREM", "4.147615", "-73.620722", 4);
-
-INSERT INTO buses (bus_name) VALUES("Terminal");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Yamaha Jordán", "4.148175" , "-73.624406", 5);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Paradero busetas COFREM", "4.147615" , "-73.620722", 5);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Paradero buses Hato Grande", "4.1470082", "-73.6168287", 5);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Frente a Olimpica", "4.1461767", "-73.6112503", 5);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Semaforo Manantial", "4.1453019", "-73.6066442", 5);
+INSERT INTO buses (bus_name) VALUES('Porfia');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Afueras bloque apartamentos', '4.0593938', '-73.6700653', 3);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Supermercado Marketing', '4.064833', '-73.671318', 3);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Salida los Chopos', '4.073257', '-73.669342', 3);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Congente', '4.079063', '-73.669262', 3);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Consuerte', '4.0826917', '-73.6692422', 3);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Bomba Texaco', '4.088458', '-73.670179', 3);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Segunda entrada Samán', '4.091312', '-73.666702', 3);
 
 
-INSERT INTO buses (bus_name) VALUES("Covisan");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Semaforo pinilla","4.1375399","-73.5892802", 6);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Bomba Terpel Covisan", "4.1371736", "-73.5863872", 6);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("El Salitre", "4.1375301", "-73.5808545", 6);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Apartamentos Amarilo", "4.1358266", "-73.5731425", 6);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("El campanario", "4.1332609", "-73.5653911", 6);
+INSERT INTO buses (bus_name) VALUES('Grama');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Cuatro Parques', '4.158367', '-73.640998', 4);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Picadas y algo más', '4.157906', '-73.637702', 4);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Frente Bomba Texaco', '4.156651', '-73.632235', 4);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Droguería la Providencia', '4.151178', '-73.628527', 4);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Yamaha Jordán', '4.148175', '-73.624406', 4);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Paradero busetas COFREM', '4.147615', '-73.620722', 4);
 
-INSERT INTO buses (bus_name) VALUES("Maracos");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Bomba los Maracos", "4.135574", "-73.6181454", 7);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Semaforo lavadero", "4.1340915", "-73.6132462", 7);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Parque Mi Llanura", "4.1326294", "-73.608517", 7);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Glorieta Terminal", "4.1303038", "-73.601359", 7);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Semaforo Pinilla Alto", "4.1296156", "-73.5894141", 7);
-
-INSERT INTO buses (bus_name) VALUES("Montecarlo");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("La Rochela", "4.1044181", "-73.6568555", 8);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Bomba Terpel Playa Rica", "4.1072854", "-73.6557697", 8);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Bahía Serramonte", "4.1155424", "-73.6490235", 8);
+INSERT INTO buses (bus_name) VALUES('Terminal');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Yamaha Jordán', '4.148175' , '-73.624406', 5);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Paradero busetas COFREM', '4.147615' , '-73.620722', 5);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Paradero buses Hato Grande', '4.1470082', '-73.6168287', 5);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Frente a Olimpica', '4.1461767', '-73.6112503', 5);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Semaforo Manantial', '4.1453019', '-73.6066442', 5);
 
 
-INSERT INTO buses (bus_name) VALUES("Parque");
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Parque de los estudiante", "4.1459132", "-73.6366358", 9);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Unicentro", "4.1432595", "-73.6334785", 9);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Tecniguayas", "4.138734", "-73.6300699", 9);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("La Industrial", "4.1352434", "-73.6273911", 9);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Makro", "4.1284916", "-73.6222394", 9);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Alborada", "4.1246844", "-73.619346", 9);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Cooperativa", "4.1156645", "-73.6095403", 9);
-INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ("Bomba Primax", "4.1108026" , "-73.6035419", 9);
+INSERT INTO buses (bus_name) VALUES('Covisan');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Semaforo pinilla','4.1375399','-73.5892802', 6);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Bomba Terpel Covisan', '4.1371736', '-73.5863872', 6);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('El Salitre', '4.1375301', '-73.5808545', 6);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Apartamentos Amarilo', '4.1358266', '-73.5731425', 6);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('El campanario', '4.1332609', '-73.5653911', 6);
+
+INSERT INTO buses (bus_name) VALUES('Maracos');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Bomba los Maracos', '4.135574', '-73.6181454', 7);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Semaforo lavadero', '4.1340915', '-73.6132462', 7);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Parque Mi Llanura', '4.1326294', '-73.608517', 7);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Glorieta Terminal', '4.1303038', '-73.601359', 7);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Semaforo Pinilla Alto', '4.1296156', '-73.5894141', 7);
+
+INSERT INTO buses (bus_name) VALUES('Montecarlo');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('La Rochela', '4.1044181', '-73.6568555', 8);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Bomba Terpel Playa Rica', '4.1072854', '-73.6557697', 8);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Bahía Serramonte', '4.1155424', '-73.6490235', 8);
+
+
+INSERT INTO buses (bus_name) VALUES('Parque');
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Parque de los estudiante', '4.1459132', '-73.6366358', 9);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Unicentro', '4.1432595', '-73.6334785', 9);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Tecniguayas', '4.138734', '-73.6300699', 9);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('La Industrial', '4.1352434', '-73.6273911', 9);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Makro', '4.1284916', '-73.6222394', 9);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Alborada', '4.1246844', '-73.619346', 9);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Cooperativa', '4.1156645', '-73.6095403', 9);
+INSERT INTO location (location_name, latitude, longitude, bus_id) VALUES ('Bomba Primax', '4.1108026' , '-73.6035419', 9);
 
 
 
-INSERT INTO hours (hour_name) VALUES ("05:00");
-INSERT INTO hours (hour_name) VALUES ("06:00");
-INSERT INTO hours (hour_name) VALUES ("07:00");
-INSERT INTO hours (hour_name) VALUES ("08:00");
-INSERT INTO hours (hour_name) VALUES ("11:00");
-INSERT INTO hours (hour_name) VALUES ("13:00");
-INSERT INTO hours (hour_name) VALUES ("11:00");
-INSERT INTO hours (hour_name) VALUES ("12:00");
-INSERT INTO hours (hour_name) VALUES ("16:00");
-INSERT INTO hours (hour_name) VALUES ("17:00");
-INSERT INTO hours (hour_name) VALUES ("18:00");
+INSERT INTO hours (hour_name) VALUES ('05:00');
+INSERT INTO hours (hour_name) VALUES ('06:00');
+INSERT INTO hours (hour_name) VALUES ('07:00');
+INSERT INTO hours (hour_name) VALUES ('08:00');
+INSERT INTO hours (hour_name) VALUES ('11:00');
+INSERT INTO hours (hour_name) VALUES ('13:00');
+INSERT INTO hours (hour_name) VALUES ('11:00');
+INSERT INTO hours (hour_name) VALUES ('12:00');
+INSERT INTO hours (hour_name) VALUES ('16:00');
+INSERT INTO hours (hour_name) VALUES ('17:00');
+INSERT INTO hours (hour_name) VALUES ('18:00');
 
 
 INSERT INTO buses_hours (bus_id, hour_id) VALUES (1, 1);
